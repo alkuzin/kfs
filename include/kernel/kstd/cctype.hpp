5a7325e0e2cecacacfbff4092a5218f63fd575be
alkuzin <alkuzindev@gmail.com>
2024-09-27 18:41:32 +0300

    feat: added isdigit() & isspace() functions

--- include/kernel/kstd/cctype.hpp
+++ include/kernel/kstd/cctype.hpp
@@ -1,25 +1,25 @@
 /**
  * Monolithic Unix-like kernel from scratch.
  * Copyright (C) 2024 Alexander (@alkuzin).
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 /**
- * @file  ctype.hpp
+ * @file  cctype.hpp
  * @brief Declares basic character handling functions.
- * 
+ *
  * @author Alexander Kuzin (<a href="https://github.com/alkuzin">alkuzin</a>)
  * @date   25.09.2024
  */
@@ -33,7 +33,7 @@ namespace kstd {
 
 /**
  * @brief Checks for a printable character (including space).
- * 
+ *
  * @param [in] c - given character.
  * @return whether c is a printable character (including space) or not.
  */
@@ -45,7 +45,7 @@ constexpr inline bool isprint(T c) noexcept
 
 /**
  * @brief Checks for an uppercase character.
- * 
+ *
  * @param [in] c - given character.
  * @return whether c is an uppercase character or not.
  */
@@ -53,6 +53,25 @@ template <typename T>
 constexpr inline bool isupper(T c) noexcept
 {
     return ((c >= 'A') && (c <= 'Z'));
+}
+
+/**
+ * @brief Checks for a digit.
+ *
+ * @param [in] c - given character.
+ * @return whether c is a digit or not.
+ */
+template <typename T>
+constexpr inline bool isdigit(T c) noexcept
+{
+    return ((c >= '0') && (c <= '9'));
+}
+
+template <typename T>
+constexpr inline bool isspace(T ch)
+{
+    return (ch == ' ' || ch == '\t' || ch == '\n' ||
+            ch == '\v' || ch == '\f' || ch == '\r');
 }
 
 } // namespace kstd
