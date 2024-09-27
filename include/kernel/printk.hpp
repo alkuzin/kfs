63fccae43620fa24a0d91d502b1df2c4f1234ea7
alkuzin <alkuzindev@gmail.com>
2024-09-27 21:52:48 +0300

    feat: added colored printk() function

--- include/kernel/printk.hpp
+++ include/kernel/printk.hpp
@@ -1,17 +1,17 @@
 /**
  * Monolithic Unix-like kernel from scratch.
  * Copyright (C) 2024 Alexander (@alkuzin).
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -19,7 +19,7 @@
 /**
  * @file  printk.hpp
  * @brief Contains function to print kernel messages.
- * 
+ *
  * @author Alexander Kuzin (<a href="https://github.com/alkuzin">alkuzin</a>)
  * @date   25.09.2024
  */
@@ -35,11 +35,21 @@ namespace kernel {
 
 /**
  * @brief Formats and prints data.
- * 
+ *
  * @param [in] fmt - given format string.
  * @param [in] ... - given variable number of arguments.
  */
 void printk(const char *fmt, ...) noexcept;
+
+/**
+ * @brief Formats and prints colored data.
+ *
+ * @param [in] fg - given foreground color.
+ * @param [in] bg - given background color.
+ * @param [in] fmt - given format string.
+ * @param [in] ... - given variable number of arguments.
+ */
+void cprintk(gfx::rgb_t fg, gfx::rgb_t bg, const char *fmt, ...) noexcept;
 
 } // namespace kernel
 
--- kernel/printk.cpp
+++ kernel/printk.cpp
@@ -1,23 +1,23 @@
 /**
  * Monolithic Unix-like kernel from scratch.
  * Copyright (C) 2024 Alexander (@alkuzin).
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <kernel/kstd/cstdio.hpp>
-#include <kernel/kstd/ctypes.hpp>
+#include <kernel/printk.hpp>
 
 
 namespace kernel {
@@ -37,7 +37,23 @@ void printk(const char *fmt, ...) noexcept
     uint32_t i = 0;
 
     while(buffer[i]) {
-        kstd::putchar(buffer[i]);       
+        kstd::putchar(buffer[i]);
+        i++;
+	}
+}
+
+void cprintk(gfx::rgb_t fg, gfx::rgb_t bg, const char *fmt, ...) noexcept
+{
+    va_list args;
+
+    va_start(args, fmt);
+    kstd::vsnprintk(buffer, BUF_SIZE, fmt, args);
+    va_end(args);
+
+    uint32_t i = 0;
+
+    while(buffer[i]) {
+        tty::terminal.putc(buffer[i], fg, bg);
         i++;
 	}
 }
