961d9f6ffed8a8589ae1043cbf65513e0cc2c5e3
alkuzin <alkuzindev@gmail.com>
2024-09-27 21:29:41 +0300

    feat: added ring() & mode() functions

--- include/kernel/arch/x86/system.hpp
+++ include/kernel/arch/x86/system.hpp
@@ -1,17 +1,17 @@
 /**
  * Monolithic Unix-like kernel from scratch.
  * Copyright (C) 2024 Alexander (@alkuzin).
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -19,7 +19,7 @@
 /**
  * @file  system.hpp
  * @brief Contains system functions.
- * 
+ *
  * @author Alexander Kuzin (<a href="https://github.com/alkuzin">alkuzin</a>)
  * @date   26.09.2024
  */
@@ -36,6 +36,36 @@ namespace x86 {
 inline void halt(void) noexcept
 {
     for (;;) __asm__ volatile("hlt");
+}
+
+/**
+ * @brief Get current privilege level .
+ *
+ * @return current privilege level.
+ */
+inline uint8_t ring(void) noexcept
+{
+    uint16_t cs;
+    __asm__ volatile ("mov %%cs, %0" : "=r"(cs)); // Read the CS register into cs
+    return cs & 0x3; // Return the lower 2 bits
+}
+
+/**
+ * @brief Get current mode.
+ *
+ * @return mode in bits.
+ */
+inline uint32_t mode(void) noexcept
+{
+    uint16_t cs;
+    __asm__ volatile ("mov %%cs, %0" : "=r"(cs));
+
+    if (cs & 0x0000FFFF)
+        return 32;
+    else if (cs & 0xFFFF0000)
+        return 64;
+    else
+        return 16;
 }
 
 } // namespace x86
