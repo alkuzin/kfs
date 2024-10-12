57f971a264ee8f6d5bca43ead975f36352e8211a
alkuzin <alkuzindev@gmail.com>
2024-10-12 12:28:05 +0300

    feat: added Interrupt Request (IRQ)

---
+++ include/kernel/arch/x86/irq.hpp
@@ -0,0 +1,154 @@
+/**
+ * Monolithic Unix-like kernel from scratch.
+ * Copyright (C) 2024 Alexander (@alkuzin).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file  irq.hpp
+ * @brief Provides definitions for Interrupt Requests (IRQ).
+ *
+ * @author Alexander Kuzin (<a href="https://github.com/alkuzin">alkuzin</a>)
+ * @date   12.10.2024
+ */
+
+#ifndef _KERNEL_ARCH_X86_IRQ_HPP_
+#define _KERNEL_ARCH_X86_IRQ_HPP_
+
+#include <kernel/linkage.hpp>
+#include <kernel/types.hpp>
+
+
+namespace kernel {
+namespace arch {
+namespace x86 {
+namespace irq {
+
+/** @brief Structure representing interrupt register state.*/
+struct int_regs_t {
+    uint32_t m_cr2;        // control register 2
+    uint32_t m_ds;         // data segment
+    uint32_t m_edi;        // destination index
+    uint32_t m_esi;        // source index
+    uint32_t m_ebp;        // base pointer
+    uint32_t m_esp;        // stack pointer
+    uint32_t m_ebx;        // base register
+    uint32_t m_edx;        // data register
+    uint32_t m_ecx;        // counter register
+    uint32_t m_eax;        // accumulator register
+    uint32_t m_int_no;     // interrupt number
+    uint32_t m_err_code;   // error code
+    uint32_t m_eip;        // instruction pointer
+    uint32_t m_csm;        // code segment
+    uint32_t m_eflags;     // flags register
+    uint32_t m_useresp;    // user stack pointer
+    uint32_t m_ss;         // stack segment
+} __attribute__((packed));
+
+// Interrupt requests handler function alias
+using handler_t = void (*)(int_regs_t*);
+
+/**
+ * @brief Install handler for IRQ.
+ *
+ * @param [in] irq - given IRQ number.
+ * @param [in] handler - given pointer to IRQ handler function.
+ */
+void request(int32_t irq, irq::handler_t handler) noexcept;
+
+/**
+ * @brief Uninstall handler for IRQ.
+ *
+ * @param [in] irq - given IRQ number.
+ */
+void free(int32_t irq) noexcept;
+
+/**
+ * @brief ISR handler function.
+ *
+ * @param [in] regs - given pointer to interrupt register state.
+ */
+asmlinkage void isr_handler(int_regs_t *regs) noexcept;
+
+/**
+ * @brief IRQ handler function.
+ *
+ * @param [in] regs - given pointer to interrupt register state.
+ */
+asmlinkage void irq_handler(int_regs_t *regs) noexcept;
+
+} // namespace irq
+
+// ISR functions for hardware interrupts 0-31
+asmlinkage void isr0(void);
+asmlinkage void isr1(void);
+asmlinkage void isr2(void);
+asmlinkage void isr3(void);
+asmlinkage void isr4(void);
+asmlinkage void isr5(void);
+asmlinkage void isr6(void);
+asmlinkage void isr7(void);
+asmlinkage void isr8(void);
+asmlinkage void isr9(void);
+asmlinkage void isr10(void);
+asmlinkage void isr11(void);
+asmlinkage void isr12(void);
+asmlinkage void isr13(void);
+asmlinkage void isr14(void);
+asmlinkage void isr15(void);
+asmlinkage void isr16(void);
+asmlinkage void isr17(void);
+asmlinkage void isr18(void);
+asmlinkage void isr19(void);
+asmlinkage void isr20(void);
+asmlinkage void isr21(void);
+asmlinkage void isr22(void);
+asmlinkage void isr23(void);
+asmlinkage void isr24(void);
+asmlinkage void isr25(void);
+asmlinkage void isr26(void);
+asmlinkage void isr27(void);
+asmlinkage void isr28(void);
+asmlinkage void isr29(void);
+asmlinkage void isr30(void);
+asmlinkage void isr31(void);
+
+// ISR functions for system calls
+asmlinkage void isr128(void);
+asmlinkage void isr177(void);
+
+// IRQ functions for hardware interrupts 0-15
+asmlinkage void irq0(void);
+asmlinkage void irq1(void);
+asmlinkage void irq2(void);
+asmlinkage void irq3(void);
+asmlinkage void irq4(void);
+asmlinkage void irq5(void);
+asmlinkage void irq6(void);
+asmlinkage void irq7(void);
+asmlinkage void irq8(void);
+asmlinkage void irq9(void);
+asmlinkage void irq10(void);
+asmlinkage void irq11(void);
+asmlinkage void irq12(void);
+asmlinkage void irq13(void);
+asmlinkage void irq14(void);
+asmlinkage void irq15(void);
+
+} // namespace x86
+} // namespace arch
+} // namespace kernel
+
+#endif // _KERNEL_ARCH_X86_IRQ_HPP_
---
+++ kernel/arch/x86/irq.cpp
@@ -0,0 +1,109 @@
+/**
+ * Monolithic Unix-like kernel from scratch.
+ * Copyright (C) 2024 Alexander (@alkuzin).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <kernel/arch/x86/system.hpp>
+#include <kernel/arch/x86/irq.hpp>
+#include <kernel/arch/x86/pic.hpp>
+#include <kernel/arch/x86/io.hpp>
+#include <kernel/panic.hpp>
+
+
+namespace kernel {
+namespace arch {
+namespace x86 {
+namespace irq {
+
+inline const char *exception_msgs[]
+{
+    "division by zero",
+    "debug",
+    "non maskable interrupt",
+    "breakpoint",
+    "into detected overflow",
+    "out of bounds",
+    "invalid opcode",
+    "no coprocessor",
+    "double fault",
+    "coprocessor segment overrun",
+    "bad TSS",
+    "segment not present",
+    "stack fault",
+    "general protection fault",
+    "page fault",
+    "unknown interrupt",
+    "coprocessor fault",
+    "alignment fault",
+    "machine check",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved",
+    "reserved"
+};
+
+// handlers that are designed to respond to hardware interrupts
+static handler_t routines[16] = {nullptr};
+
+void request(int32_t irq, handler_t handler) noexcept
+{
+    routines[irq] = handler;
+    sti();
+}
+
+void free(int32_t irq) noexcept
+{
+    routines[irq] = 0;
+}
+
+asmlinkage void irq_handler(int_regs_t *regs)
+{
+    // IRQ handler processes the interrupt by calling the appropriate
+    // handler function based on the interrupt number
+    handler_t handler = routines[regs->m_int_no - 32];
+
+    // handle interrupt if handler exists
+    if(handler)
+        handler(regs);
+
+    // this tells the slave PIC that interrupt handling was finished
+    if(regs->m_int_no >= 40)
+        outb(pic::SLAVE_PIC_CMD, pic::END_OF_INTERRUPT);
+
+    // this tells the master PIC that interrupt handling was finished
+    outb(pic::MASTER_PIC_CMD, pic::END_OF_INTERRUPT);
+}
+
+asmlinkage void isr_handler(int_regs_t *regs)
+{
+    // handle exceptions
+    if(regs->m_int_no < 32)
+        panic("%s\n", exception_msgs[regs->m_int_no]);
+}
+
+} // namespace irq
+} // namespace x86
+} // namespace arch
+} // namespace kernel
