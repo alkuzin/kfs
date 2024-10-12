cf14d06f6d992a97d25bd5ab03df55b60a055e1d
alkuzin <alkuzindev@gmail.com>
2024-10-12 12:26:30 +0300

    feat: added Programmable Interrupt Controller

---
+++ include/kernel/arch/x86/pic.hpp
@@ -0,0 +1,67 @@
+/**
+ * Monolithic Unix-like kernel from scratch.
+ * Copyright (C) 2024 Alexander (@alkuzin).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file  pic.hpp
+ * @brief Provides definitions for Programmable Interrupt Controller (PIC).
+ *
+ * @author Alexander Kuzin (<a href="https://github.com/alkuzin">alkuzin</a>)
+ * @date   12.10.2024
+ */
+
+#ifndef _KERNEL_ARCH_X86_PIC_HPP_
+#define _KERNEL_ARCH_X86_PIC_HPP_
+
+#include <kernel/types.hpp>
+
+
+namespace kernel {
+namespace arch {
+namespace x86 {
+namespace pic {
+
+// 8259 PIC input/output ports:
+inline const uint8_t MASTER_PIC_CMD             {0x20};
+inline const uint8_t MASTER_PIC_DATA            {0x21};
+inline const uint8_t SLAVE_PIC_CMD              {0xA0};
+inline const uint8_t SLAVE_PIC_DATA             {0xA1};
+
+// PIC configurations:
+inline const uint8_t PIC_INIT_CMD               {0x11};
+inline const uint8_t MASTER_PIC_VECTOR_OFFSET   {0x20};
+inline const uint8_t SLAVE_PIC_VECTOR_OFFSET    {0x28};
+inline const uint8_t MASTER_PIC_CASCADE         {0x04};
+inline const uint8_t SLAVE_PIC_CASCADE          {0x02};
+inline const uint8_t PIC_8086_MODE              {0x01};
+inline const uint8_t ALL_INTERRUPTS_ENABLED     {0x00};
+inline const uint8_t END_OF_INTERRUPT           {0x20};
+
+struct pic_t
+{
+    /** @brief Initialize Programmable Interrupt Controller (PIC).*/
+    void init(void) noexcept;
+};
+
+extern pic_t pic;
+
+} // namespace pic
+} // namespace x86
+} // namespace arch
+} // namespace kernel
+
+#endif // _KERNEL_ARCH_X86_PIC_HPP_
---
+++ kernel/arch/x86/pic.cpp
@@ -0,0 +1,56 @@
+/**
+ * Monolithic Unix-like kernel from scratch.
+ * Copyright (C) 2024 Alexander (@alkuzin).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <kernel/arch/x86/pic.hpp>
+#include <kernel/arch/x86/io.hpp>
+
+
+namespace kernel {
+namespace arch {
+namespace x86 {
+namespace pic {
+
+void pic_t::init(void) noexcept
+{
+    // send the initialization command to both the master and slave PICs
+    outb(MASTER_PIC_CMD, PIC_INIT_CMD);
+    outb(SLAVE_PIC_CMD,  PIC_INIT_CMD);
+
+    // set the vector offsets for the master and slave PICs
+    outb(MASTER_PIC_DATA, MASTER_PIC_VECTOR_OFFSET);
+    outb(SLAVE_PIC_DATA,  SLAVE_PIC_VECTOR_OFFSET);
+
+    // configure the master and slave PICs for cascade operation
+    outb(MASTER_PIC_DATA, MASTER_PIC_CASCADE);
+    outb(SLAVE_PIC_DATA,  SLAVE_PIC_CASCADE);
+
+    // set both PICs to operate in 8086/8088 mode
+    outb(MASTER_PIC_DATA, PIC_8086_MODE);
+    outb(SLAVE_PIC_DATA,  PIC_8086_MODE);
+
+    // enable all interrupts on both PICs
+    outb(MASTER_PIC_DATA, ALL_INTERRUPTS_ENABLED);
+    outb(SLAVE_PIC_DATA,  ALL_INTERRUPTS_ENABLED);
+}
+
+pic_t pic;
+
+} // namespace pic
+} // namespace x86
+} // namespace arch
+} // namespace kernel
