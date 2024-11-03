a3ebab60e8353339112d71c0675b38dff5bffc07
alkuzin <alkuzindev@gmail.com>
2024-11-03 22:54:15 +0300

    feat: added cube rotation builtin demo app

--- kernel/shell/builtin.cpp
+++ kernel/shell/builtin.cpp
@@ -16,12 +16,14 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <kernel/drivers/keyboard.hpp>
 #include <kernel/shell/builtin.hpp>
 #include <kernel/arch/x86/gdt.hpp>
 #include <kernel/arch/x86/cpu.hpp>
 #include <kernel/kstd/cstring.hpp>
 #include <kernel/drivers/pit.hpp>
 #include <kernel/arch/x86/io.hpp>
+#include <kernel/kstd/cmath.hpp>
 #include <kernel/gfx/tui.hpp>
 #include <kernel/config.hpp>
 #include <kernel/debug.hpp>
@@ -243,6 +245,193 @@ static void shutdown(int32_t argc, char **argv) noexcept
     arch::x86::outw(0x4004, 0x3400); // for VirtualBox
 }
 
+// Rotating 3D cube demo ----------------------------------------------------
+
+struct vertice_t {
+    float32_t x;
+    float32_t y;
+    float32_t z;
+};
+
+inline const vertice_t cube_vertices[8] {
+    {-1.0f, -1.0f, -1.0f},
+    { 1.0f, -1.0f, -1.0f},
+    { 1.0f,  1.0f, -1.0f},
+    {-1.0f,  1.0f, -1.0f},
+    {-1.0f, -1.0f,  1.0f},
+    { 1.0f, -1.0f,  1.0f},
+    { 1.0f,  1.0f,  1.0f},
+    {-1.0f,  1.0f,  1.0f}
+};
+
+inline const int32_t cube_edges[12][2] {
+    {0, 1}, {1, 2}, {2, 3}, {3, 0},
+    {4, 5}, {5, 6}, {6, 7}, {7, 4},
+    {0, 4}, {1, 5}, {2, 6}, {3, 7}
+};
+
+// shift from upper-left corner of the screen
+inline const int32_t SCREEN_SHIFT {300};
+
+/**
+ * @brief Project 3D points to 2D.
+ *
+ * @param [in] vert - given cube vertice.
+ * @param [out] point - given point that holds projected x & y positions.
+ * @param [in] angle - given rotation angle.
+ */
+static void project(const vertice_t& vert, gfx::point_t& point, float32_t angle) noexcept
+{
+    float32_t scale     = 200.0f; // scale factor for projection
+    float32_t z_offset  = 3.0f;   // distance from the viewer
+
+    // applying rotation around the Y-axis
+    float32_t cos_angle = kstd::cos(angle);
+    float32_t sin_angle = kstd::sin(angle);
+    float32_t rotated_x = vert.x * cos_angle - vert.z * sin_angle;
+    float32_t rotated_z = vert.x * sin_angle + vert.z * cos_angle;
+
+    // perspective projection
+    auto center_x = scale * rotated_x / (rotated_z + z_offset);
+    auto center_y = scale * vert.y / (rotated_z + z_offset);
+
+    point.x = static_cast<int32_t>(center_x) + 160;
+    point.y = static_cast<int32_t>(center_y) + 120;
+}
+
+/**
+ * @brief Draw the cube.
+ *
+ * @param [in] angle - given cube rotation angle.
+ */
+static void draw_cube(float32_t angle, gfx::rgb_t color) noexcept
+{
+    gfx::point_t p1, p2;
+    int32_t      v1, v2;
+
+    for (int i = 0; i < 12; i++) {
+        v1 = cube_edges[i][0];
+        v2 = cube_edges[i][1];
+
+        // projecting the vertices to 2D
+        project(cube_vertices[v1], p1, angle);
+        project(cube_vertices[v2], p2, angle);
+
+        // drawing the line between the projected points
+        gfx::draw_line(p1 + SCREEN_SHIFT, p2 + SCREEN_SHIFT, color);
+    }
+}
+
+/**
+ * @brief Get the next RGB color.
+ *
+ * @param [in] color - given color.
+ * @return next color relative to the given one.
+ */
+static gfx::rgb_t get_next_color(gfx::rgb_t color) noexcept
+{
+    uint8_t r = (color >> 16) & 0xFF;
+    uint8_t g = (color >> 8) & 0xFF;
+    uint8_t b = color & 0xFF;
+
+    if (r == 255 && g < 255 && b == 0)
+        g+=3;
+    else if (g == 255 && r > 0 && b == 0)
+        r-=3;
+    else if (g == 255 && b < 255)
+        b+=3;
+    else if (b == 255 && g > 0)
+        g+=3;
+    else if (b == 255 && r < 255)
+        r+=3;
+    else if (r == 255 && g == 0 && b == 0)
+        return 0xFF0000;
+    else
+        return 0xFF0000;
+
+    return (0xFF << 24) | (r << 16) | (g << 8) | b;
+}
+
+/**
+ * @brief Single cube rotating iteration.
+ *
+ * @param [in] angle - given cube rotation angle.
+ * @param [in] color - given cube color.
+ */
+static void interation(float32_t angle, gfx::rgb_t *color) noexcept
+{
+    using namespace driver;
+    gfx::rgb_t new_color;
+
+    // update terminal cursor position
+    tty::terminal.x_pos = tty::terminal.begin_x_pos;
+    tty::terminal.y_pos = tty::terminal.begin_y_pos;
+
+    // clearing the screen
+    auto shift  = static_cast<uint32_t>(SCREEN_SHIFT);
+    auto bg_col = tty::terminal.bg;
+
+    for (uint32_t y = 0; y < shift; y++) {
+        for (uint32_t x = 0; x < shift; x++)
+            vesa::draw_pixel(x + SCREEN_SHIFT, y + SCREEN_SHIFT - 50, bg_col);
+    }
+
+    // update color
+    new_color = get_next_color(*color);
+    draw_cube(angle, new_color);
+    *color = new_color;
+}
+
+/** @brief Display rounding cube.*/
+static void round_cube(void) noexcept
+{
+    using namespace driver;
+    float32_t angle           = 0.0f;
+    float32_t angle_increment = 0.05f; // Increment angle for rotation
+
+    gfx::rgb_t color = 0xFF0000; // red color
+    keyboard::KEY key;
+
+    for (;;) {
+        key = keyboard::getch();
+
+        switch (key) {
+        case keyboard::KEY::A:
+            // updating the angle for the next frame
+            angle -= angle_increment;
+
+            // at some point the drawing of the cube becomes incorrect
+            // so that code preventing this case
+            if (static_cast<int32_t>(angle) <= 0)
+                angle = 32;
+
+            interation(angle, &color);
+            break;
+
+        case keyboard::KEY::D:
+            // updating the angle for the next frame
+            angle += angle_increment;
+
+            // at some point the drawing of the cube becomes incorrect
+            // so that code preventing this case
+            if (static_cast<int32_t>(angle) >= 32)
+                angle = 0;
+
+            interation(angle, &color);
+            break;
+
+        case keyboard::KEY::ESC:
+            return;
+
+        default:
+            break;
+        }
+
+    }
+}
+
+// --------------------------------------------------------------------------
+
 static void tui(int32_t argc, char **argv) noexcept
 {
     (void)argc; (void)argv; // unused
@@ -261,7 +450,17 @@ static void tui(int32_t argc, char **argv) noexcept
         auto window = static_cast<window_t*>(wptr);
         window->destroy();
         tty::clear();
-        printk(KERN_ERR "%s\n", "not implemented...");
+
+        kstd::putk("press 'A' to rotate cube in left direction\n");
+        kstd::putk("press 'D' to rotate cube in right direction\n");
+        kstd::putk("press 'ESC' key to exit\n");
+        auto begin_x_pos = tty::terminal.x_pos;
+        auto begin_y_pos = tty::terminal.y_pos;
+
+        round_cube();
+
+        tty::terminal.x_pos = begin_x_pos;
+        tty::terminal.y_pos = begin_y_pos;
     };
 
     auto reject_on_click = [](void *wptr) {
@@ -270,7 +469,7 @@ static void tui(int32_t argc, char **argv) noexcept
     };
 
     window.add_button("< YES >", submit_on_click, &window, {100, 225});
-    window.add_button("< NO >", reject_on_click, &window, {200, 225});
+    window.add_button("< NO >",  reject_on_click, &window, {200, 225});
     window.show();
 }
 
