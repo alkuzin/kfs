4b40bcc0c21b732075fc4b5e7e91b5968db96a48
alkuzin <alkuzindev@gmail.com>
2024-11-09 21:52:52 +0300

    refactor: enabled paging & switched to the higher half

--- boot/boot.asm
+++ boot/boot.asm
@@ -19,9 +19,8 @@
 .set MBOOT_MAGIC, 0x1BADB002
 
 # flags can control various functions of the BIOS such as:
-# enabling/disabling specific hardware components
-# setting up boot options
-# adjusting power management settings
+# enabling/disabling specific hardware components, setting up boot options,
+# adjusting power management settings and etc.
 .set MBOOT_PAGE_ALIGN, 1 << 0 # memory should be page-aligned to 4 KB
 .set MBOOT_MEM_INFO,   1 << 1 # request memory information from the bootloader
 .set MBOOT_USE_GFX,    1 << 2 # enable graphics mode
@@ -34,16 +33,19 @@
 .set MBOOT_CHECKSUM, -(MBOOT_MAGIC + MBOOT_FLAGS)
 
 .section .multiboot
-.align 4                    # aligns next data element/instruction that is multiple of 4 bytes
-    .long MBOOT_MAGIC       # declare double word (32-bit) of Magic number
-    .long MBOOT_FLAGS       # declare double word (32-bit) of Magic number flags
-    .long MBOOT_CHECKSUM    # declare double word (32-bit) of Magic number checksum
-    .long 0, 0, 0, 0, 0
+.align 4
+    .long MBOOT_MAGIC       # declare double word of magic number
+    .long MBOOT_FLAGS       # declare double word of magic number flags
+    .long MBOOT_CHECKSUM    # declare double word of magic number checksum
+    .long 0, 0, 0, 0, 0     # declare double words for some fields
 
     .long 0                 # linear graphics mode
     .long 1024              # screen width
     .long 768               # screen height
     .long 32                # depth
+
+# higher-half kernel virtual base address (3 GB)
+.set base_address, 0xC0000000
 
 .section .bss               # stores uninitialized global and static variables
 .align 16                   # reserving space for the stack
@@ -55,11 +57,36 @@ stack_top:
 
 .global boot
 boot:
-    mov $stack_top, %esp    # set stack pointer
-    push %ebx               # multiboot info
-    push %eax               # magic number
+    # move initial page directory address to ESI, ECX & EDX
+    mov $(initial_page_dir - base_address), %esi
+    mov %esi, %ecx
+    mov %esi, %edx
+
+    or $0x00000003, %ecx    # set the attributes: read/write & present bits
+    mov %ecx, 0xFFC(%edx)   # initial page directory address + 4092
+    mov %esi, %cr3          # update the page directory
+
+    mov %cr4, %ecx          # get the control register 4 value
+    or $0x00000010, %ecx    # set the PSE (Page Size Extension) bit
+    mov %ecx, %cr4          # update CR4 to enable 4 MB pages
+
+    mov %cr0, %ecx          # get the control register 0 value
+    or $0x80000000, %ecx    # set the paging bit
+    mov %ecx, %cr0          # update CR0 to enable paging
+
+    call higher_half
+
+higher_half:
+    mov %cr0, %ecx          # get the control register 0 value
+    or $1, %cl              # set the protection mode bit
+    mov %ecx, %cr0          # update the control register 0
+
+    mov $stack_top, %esp    # set the stack pointer
+    push %ebx               # push multiboot info
+    push %eax               # push magic number
     xor %ebp, %ebp          # reset ebp
-    .extern kmain           # from kernel/lernel.cpp
+
+    .extern kmain           # from kernel/kernel.cpp
     call kmain              # call kernel entry point
     cli                     # clear interrupt flag (disables interrupts)
 
@@ -69,3 +96,22 @@ halt:
 
 .section .data              # stores initialized global and static variables
 .align 0x1000
+
+.global initial_page_dir    # used for enabling paging in early boot
+initial_page_dir:
+# identity map first 3 GB of virtual memory (0x00000000-0xC0000000)
+.set i, 0x00000083          # 4 MB page is present & allowed to read/write
+.rept 0x300                 # 0x300 = 0xbfc00000/0x00400000 + 1
+    .long i
+    .set i, i + 0x00400000  # 4 MB
+.endr
+
+# map first 4 MB page to (0xC0000000-0xC0400000) from (0x00000000-0x00400000)
+.long 0x00000083            # 4 MB page is present & allowed to read/write
+
+# identity map addresses (0xC0400000-0xFFFFFFFF)
+.set i, 0xC0400083
+.rept 0xFF                  # 0xFF = (0xffc00000 - 0xc0400000)/0x00400000 + 1
+    .long i
+    .set i, i + 0x00400000  # 4 MB
+.endr
